2d48a9a3e0e1a891dccaecd4e69f117463f6c136
Yegor Bugayenko <yegor@tpc2.com>
2016-02-25 19:35:17 -0800

    attrs

--- make-sitemap.rb
+++ make-sitemap.rb
@@ -6,7 +6,12 @@ require 'date'
 
 today = Date.today.strftime('%Y-%m-%dT00:00:00+00:00')
 xml = Nokogiri::XML::Builder.new do |xml|
-  xml.urlset do
+  attrs = {
+    'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+    'xsi:schemaLocation' => 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd',
+    'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9'
+  }
+  xml.urlset(attrs) do
     Dir['target/**/*.html'].each do |file|
       xml.url do |url|
         url.loc file.gsub(/^target\//, 'http://www.seedramp.com/')
