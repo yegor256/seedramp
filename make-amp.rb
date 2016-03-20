ab1605ea85969e23382979a0c6af78d0b2dfeb62
Yegor Bugayenko <yegor@tpc2.com>
2016-03-19 18:24:36 -0700

    better AMP, with styles

--- make-amp.rb
+++ make-amp.rb
@@ -31,45 +31,25 @@ File.write(
   Xembly::Xembler.new(
     Xembly::Directives.new(
       %{
-      XPATH "/html";
-      ATTR "amp", "";
-      ATTR "lang", "en";
-      ADD "link";
-      ATTR "rel", "canonical";
-      ATTR "href", "http://www.seedramp.com#{name}.html";
+      XPATH "/html"; ATTR "amp", ""; ATTR "lang", "en";
+      XPATH "head";
+
+      ADD "style";
+      ATTR "amp-boilerplate", "";
+      SET "body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}";
       UP;
-      ADD "script";
-      ATTR "async", "";
+
+      ADD "noscript"; ADD "style";
+      ATTR "amp-boilerplate", "";
+      SET "body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}";
+      UP; UP;
+
+      ADD "script"; ATTR "async", "";
       ATTR "src", "https://cdn.ampproject.org/v0.js";
+      SET "// empty";
+      UP;
       }
     )
   ).apply(File.read(html))
 )
 
-# File.write(
-#   "#{name}.amp.html",
-#   Xembly::Xembler.new(
-#     Xembly::Directives.new(
-#       %{
-#       XPATH "/html";
-#       ATTR "amp", "";
-#       ADD "style";
-#       ATTR "amp-boilerplate", "";
-#       SET "body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}";
-#       UP;
-#       ADD "noscript";
-#       ADD "style";
-#       ATTR "amp-boilerplate", "";
-#       SET "body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}";
-#       UP; UP;
-#       ADD "link";
-#       ATTR "rel", "canonical";
-#       ATTR "href", "http://www.seedramp.com#{name}.html";
-#       UP;
-#       ADD "script";
-#       ATTR "async", "";
-#       ATTR "src", "https://cdn.ampproject.org/v0.js";
-#       }
-#     )
-#   ).apply(File.read(html))
-# )
